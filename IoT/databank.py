1b136317ccd484d17497b728866403db2fc46f36
johanneskirmayr <kirmayrj@gmail.com>
2022-12-16 19:40:51 +0100

    Added mqtt client -- not tested

--- IoT/databank.py
+++ IoT/databank.py
@@ -4,9 +4,9 @@ class DataBank:
     def __init__(self):
         # hardcoded databank of UUIDs and their locations
         self.databank = {
-            "11111111-1111-1111-1111-111111111111": {"floor": 1, "room": "A", "voice_message": "You are at the start"},
-            "22222222-2222-2222-2222-222222222222": {"floor": 2, "room": "B", "voice_message": "TODO"},
-            "33333333-3333-3333-3333-333333333333": {"floor": 3, "room": "C", "voice_message": "TODO"},
+            "8882358095064d379d5b437e33a09e8c": {"floor": 1, "room": "A", "voice_message": "You are at the start"},
+            "2a7f6ba12ff44a6aa74df05abc062b0a": {"floor": 2, "room": "B", "voice_message": "TODO"},
+            "9912e0beec0b4fd3854eb6fca1501669": {"floor": 3, "room": "C", "voice_message": "TODO"},
         }
 
     def get_location(self, uuid):
---
+++ IoT/mqtt.py
@@ -0,0 +1,60 @@
+# Create a class called MQTTClient that will be used to send and receive messages from the broker. 
+# The class will have the following methods: send_message, receive_message, and connect.
+
+from paho.mqtt import client as mqtt_client
+from queue import Queue
+
+
+class MQTTClient:
+    def __init__(self, client_id, broker_ip, broker_port):
+        # Create a client instance
+        self.client = mqtt_client.Client(client_id)
+        # Set the broker IP and port
+        self.broker_ip = broker_ip
+        self.broker_port = broker_port
+        # Set username and password
+        #self.username = "mqtt_user"
+        #self.password = "mqtt_password"
+        # Create a queue to store received messages
+        self.message_queue = Queue()
+
+    def connect(self):
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
+        
+        # Set the username and password
+        #self.client.username_pw_set(self.username, self.password)
+
+        # Set the callback function
+        self.client.on_connect = on_connect
+
+        # Connect to the broker
+        self.client.connect(self.broker_ip, self.broker_port)
+
+        return self.client
+
+    def send_message(self, topic, message):
+        # Send a message to the broker
+        result = self.client.publish(topic, message)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{message}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+
+    def receive_message(self, topic):
+        # Receive a message from the broker
+        self.client.subscribe(topic)
+        self.client.on_message = self.on_message
+        self.client.loop_start()
+
+    def on_message(self, client, userdata, message):
+        # Callback function for when a message is received
+        self.message_queue.put(message)
+    
+        # print("Received message '" + str(message.payload) + "' on topic '"
+        #       + message.topic + "' with QoS " + str(message.qos))
