744fbd79caccb913dfc8aa7c62bac9023be79769
johanneskirmayr <kirmayrj@gmail.com>
2023-01-07 10:49:46 +0100

    removed obsolete BT UUIDs

--- IoT/uuidDatabank.py
+++ IoT/uuidDatabank.py
@@ -13,10 +13,6 @@ class DataBank:
             "9912e0beec0b4fd3854eb6fca1501669": {"floor": 3, "room": "D", "previous_room": "B", "voice_message": "", "bridge_voice_message": "You next stop is already your destination, room number D!"},
             "0c87386ed6cf481b8a7393c075d85d43": {"floor": 3, "room": "E", "previous_room": "B", "voice_message": "", "bridge_voice_message": "You next stop is already your destination, room number E!"},
             "1f2437baded74f9ea308d7bf06a8a406": {"floor": 3, "room": "F", "previous_room": "C", "voice_message": "", "bridge_voice_message": "You next stop is already your destination, room number F!"},
-            
-            #"3700ad2975ee4ec999fe9a8e27192c3a": {"floor": 4, "room": "D", "previous_room":, "voice_message": "Test Beacon 1"},
-            #"d369a9b2a854459e8e9d5403ee320eef": {"floor": 5, "room": "E", "previous_room":, "voice_message": "Test Beacon 2"}
-
         }
 
     def get_location(self, uuid):
